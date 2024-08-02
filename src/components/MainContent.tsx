223e7406a0cf591b6f04de9da05dc28b21f872b6
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-08-02 13:03:01 +0200

    make dropdown functional

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -109,7 +109,10 @@ const MainContent = () => {
             <div className="mb-5">
                 <div className="flex flex-col sm:flex-row justify-between items-center">
                     <div className="relative mb-5 mt-5">
-                        <button className="border px-4 py-2 rounded-full flex items-center">
+                        <button
+                            onClick={() => setDropdownOpen(!dropdownOpen)}
+                            className="border px-4 py-2 rounded-full flex items-center"
+                        >
                             <Tally3 className="mr-2" />
 
                             {filter === "all"
@@ -189,6 +192,7 @@ const MainContent = () => {
                         Next
                     </button>
                 </div>
+                {/* end of pagination */}
             </div>
         </section>
     );
